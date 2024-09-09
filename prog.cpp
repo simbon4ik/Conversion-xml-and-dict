6fa93ba53a30ea636f2c8e10f05666261f170fd9
karpov.is <karpov.is@samos.dozen.mephi.ru>
2024-09-09 22:26:13 +0300

    add function overloading

--- prog.cpp
+++ prog.cpp
@@ -17,18 +17,47 @@ struct dict
         int emphasis;
 };
 
-dict conversion_second(string input);
 
-int conv_s_dialog(){                    //string
+dict conversion_second(string input);
+dict conversion_second(const char* input);              //const char*
+dict conversion_second(const char* symbols, size_t length);
+
+
+int conv_s_dialog(){
         string input;
         cout << "Enter xml string (format: <record> <word>apple</word> <meaning>round fruit</meaning> <emphasis>1</emphasis> </record>" << endl;
         getline(cin, input);
+        
+        //1const char* input = "<record> <word>apple</word> <meaning>round fruit</meaning> <emphasis>1</emphasis> </record>";   for test const char*
+        
+        //2const char symbols[] = {'<','r','>','<','w','o','r','d','>','a','<','/','w','o','r','d','>','<','m','e','a','n','i','n','g','>','r',' ','f','<','/','m','e','a','n','i','n','g','>','<','e','m','p','h','a','s','i','s','>','1','<','/','e','m','p','h','a','s','i','s','<','/','r','>'};
+        //2size_t length = 64; 
+        //2dict result = conversion_second(symbols, length);            for test const char* + size_t 
+
         dict result = conversion_second(input);
         cout << "Result is:\nword = " << result.word << "\nmeaning = " << result.meaning << "\nemphasis = " << result.emphasis << endl;
         return 0;
 }
 
-dict conversion_second(string input){
+        
+dict conversion_second(const char* input){              //const char*
+        string input_mod = input;
+        return conversion_second(input_mod);
+}
+
+
+dict conversion_second(const char* symbols, size_t length){
+        char* input = new char[length+1];
+        for (int i = 0; i < length; i++)
+                input[i] = symbols[i];
+        input[length] = '\0';
+        string input_mod = input;
+        delete[] input;
+        return conversion_second(input_mod);
+}
+
+
+dict conversion_second(string input){                   //string
         dict result;
         int index = input.find("word") + 5;
         int index_2 = input.find("<",index); 
@@ -45,6 +74,7 @@ dict conversion_second(string input){
         
         return result;
 }
+
 
 string conversion_first(dict item, string name);
 
@@ -66,13 +96,10 @@ int conv_f_dialog(){
 }
 
 
-
-
 string conversion_first(dict item, string name){
         string result = '<' + name + '>' + "\n\t<word>" + item.word + "</word>\n\t<meaning>" + item.meaning + "<meaning>\n\t<emphasis>" +std::to_string(item.emphasis) + "</emphasis>\n</"+ name + ">\n";
         return result;
 }
-
 
 
 int main(){
